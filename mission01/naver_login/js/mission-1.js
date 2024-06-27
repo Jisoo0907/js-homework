3fbe3ce33b05002a2ce8a985e65bdac8f3b0edb0
jisoolee <szjslee@gmail.com>
2024-06-27 18:30:28 +0900

    mission-2 과제 다운

--- mission01/naver_login/js/mission-1.js
+++ mission01/naver_login/js/mission-1.js
@@ -54,3 +54,59 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   userValidation(emailInput, pwInput);
 });
+
+/* 진혁님 코드 */
+const $emailInput = document.querySelector("#userEmail");
+const $passwordInput = document.querySelector("#userPassword");
+const $loginBtn = document.querySelector(".btn-login");
+
+let emailCheckPass = false;
+let pwCheckPass = false;
+
+function handleEmailCheck() {
+  const value = this.value;
+
+  if (emailReg(value)) {
+    this.classList.remove("is--invalid");
+    emailCheckPass = true;
+  } else {
+    this.classList.add("is--invalid");
+    emailCheckPass = false;
+  }
+}
+
+function handlePasswordCheck() {
+  const value = this.value;
+
+  if (pwReg(value)) {
+    this.classList.remove("is--invalid");
+    pwCheckPass = true;
+  } else {
+    this.classList.add("is--invalid");
+    pwCheckPass = false;
+  }
+}
+
+function handleLogin(e) {
+  e.preventDefault();
+
+  if (emailCheckPass && pwCheckPass) {
+    try {
+      const id = $emailInput.value;
+      const pw = $passwordInput.value;
+
+      const getUserId = user.id;
+      const getUserPw = user.pw;
+
+      if (id === getUserId && pw === getUserPw) {
+        location.href = "welcome.html";
+      }
+    } catch {
+      // 통신 했는데 아이디 없다, 서버가 뻗었다 등등 에러 처리
+    }
+  }
+}
+
+$emailInput.addEventListener("input", handleEmailCheck);
+$passwordInput.addEventListener("input", handlePasswordCheck);
+$loginBtn.addEventListener("click", handleLogin);
